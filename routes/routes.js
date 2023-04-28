cb5952b349caa7e30a1c93c53dfff3ec9c3ecf92
Isaac Isvaldo <91834847+IsaacIsvaldo@users.noreply.github.com>
2023-04-28 14:34:42 +0200

    adicionei oas viws

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,5 @@ const UserController =require ('../controllers/Usernormal')
 
 //Home Page Digital
 router.get('/' ,UserController.index); 
+router.get('/Dashbord_user' ,UserController.Dashbord_user); 
 module.exports = router;
