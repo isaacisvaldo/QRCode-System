359b70b35be5dfaa352a853b7b46f01361f982b2
Isaac Isvaldo <isaacisvaldobunga300@gmail.com>
2023-05-04 14:44:40 +0100

    Adicionei o script de cadstro do admin

--- script/script.js
+++ script/script.js
@@ -1,7 +1,28 @@
 const cron = require('node-cron');
-const BD = require('../config/database')
+const BD = require('../databases/database')
+const bcrypt = require('bcryptjs');
 
-cron.schedule('* * * * * *', () => {
-    console.log('Rodar em Cada minuto');
+
+const email = process.env.EMAIL_ADMIN || "isaacisvaldobunga300@gmail.com";
+const user =process.env.USER_ADMIN  ||'isvaldo';
+    const senha = '12345678';
+    var salt = bcrypt.genSaltSync(10);
+    var senha_admin = bcrypt.hashSync(senha, salt);
+
+cron.schedule('*/60 * * * *',  async() => {
+    
+    const admin = await BD("admin")
+    .where("email_admin", email)
+    .orWhere("username_admin", user)
+    .first();
+
+if(admin){
+    console.log('Encontrei o Admin');
+
+}else{
+   const admin = await BD("admin").insert({ image_admin:'user.png', username_admin:'isvaldo', nome_admin:'Isaac Isvaldo Bunga', email_admin:'isaacisvaldobunga300@gmail.com', telefone_admin:'930333042', senha_admin})
+   console.log('Administrador Cadastrado !');
+    console.log(admin)
+}
 });
     
